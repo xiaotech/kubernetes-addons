078295f71ae313ff11c6aeb291cae1c6c56d9293
jun.xiao <jun.xiao@sihuatech.com>
2019-03-04 16:31:58 +0800

    ha

--- ha/cert_scp.sh
+++ ha/cert_scp.sh
@@ -1,6 +1,7 @@
 USER=root # customizable
 CONTROL_PLANE_IPS="172.30.81.193 172.30.81.194"
 for host in ${CONTROL_PLANE_IPS}; do
+    ssh ${USER}@$host test -d  /etc/kubernetes/pki/etcd ||  ssh ${USER}@$host mkdir  /etc/kubernetes/pki/etcd -p
     scp /etc/kubernetes/pki/ca.crt "${USER}"@$host:/etc/kubernetes/pki
     scp /etc/kubernetes/pki/ca.key "${USER}"@$host:/etc/kubernetes/pki
     scp /etc/kubernetes/pki/sa.key "${USER}"@$host:/etc/kubernetes/pki
